9f3936337493d4b3823e3b2d7826e48da5d60966
Khattak <akkhattak65@gmail.com>
2020-05-16 14:43:57 +0500

    :art: Restructured Layout

--- index.js
+++
@@ -1,7 +0,0 @@
-const breakPointButton = document.querySelector(".btn-1");
-const breakPointOnAttributeChange = document.querySelector(".btn-2");
-
-breakPointButton.addEventListener("click", (e) => {
-  breakPointOnAttributeChange.style.background = "#1abc9c";
-  breakPointOnAttributeChange.style.border = "3px solid #1abc9c";
-});
