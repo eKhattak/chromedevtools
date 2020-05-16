65f59287fa11018679e506e94c0f9a83e3fd5df7
Khattak <akkhattak65@gmail.com>
2020-05-16 15:04:02 +0500

    �:bulb: Added About

---
+++ about.js
@@ -0,0 +1,18 @@
+// I am
+// 👨  Arsalan Khattak
+
+// Study at
+// 🏫  Numl Islamabad
+
+// Work at
+// 🏢  CARE Pvt Ltd
+
+// Technical Lead at
+// 👨‍💻  MSP - Islamabad
+
+// Available at
+// 🔗  https://twitter.com/khattakdev
+
+// 🔗  https://www.linkedin.com/in/arsalan-dilawar-56375a167/
+
+// 🔗  https://github.com/eKhattak
